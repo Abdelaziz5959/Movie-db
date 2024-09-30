71f88f2d63408ebb2d908a3b90e84c35be14437a
Abdelaziz590 <159035240+Abdelaziz590@users.noreply.github.com>
2024-09-30 12:32:51 +0200

    first commit

---
+++ src/Pages/GenresPage.jsx
@@ -0,0 +1,8 @@
+const GenresPages = () => {
+    return <>
+
+    <h1>Genre</h1>
+    </>;
+}
+ 
+export default GenresPages;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return <>
+    <h1>Page d&apos;acceuil</h1>
+    </>;
+}
+ 
+export default HomePage;
